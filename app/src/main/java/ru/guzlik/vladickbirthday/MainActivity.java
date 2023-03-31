8a28b000c20ef88b99f34d0a194d7c103e080cf8
GUZLIKE <vinnickow.e@yandex.ru>
2023-04-01 02:31:22 +0300

    ADDED PICTURE & TOAST

--- app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
+++ app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
@@ -3,18 +3,32 @@ package ru.guzlik.vladickbirthday;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    View mainView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showText(mainView);
+
+
+
+
     }
 
 
-    public static void hello(){
-        System.out.printf("asdsadfdga");
-        int i = 0;
+    public void showText(View view) {
+        Toast.makeText(this, "О! ЗДАРОВА ЧЕЛИК! ТУТ КОРОЧЕ НАДО ОДНОГО ЧУВАКА СОБРАТЬ" +
+                "ПОШЛИ, ПОМОЖЕШЬ!", Toast.LENGTH_LONG).show();
     }
+
+
+
 }
