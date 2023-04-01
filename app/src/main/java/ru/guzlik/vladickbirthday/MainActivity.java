d990ff0806547a81c7dee00e36d92f1989c9d2fe
GUZLIKE <vinnickow.e@yandex.ru>
2023-04-01 03:11:00 +0300

    ADDED TEXT & SECOND ACTIVITY

--- app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
+++ app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
@@ -2,32 +2,54 @@ package ru.guzlik.vladickbirthday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView text;
+    Button button;
 
-    View mainView;
+    private String[] textArray = {"О! ЗДОРОВА! ТУТ ОДНОГО ЧЕЛА НАДО СОБРАТЬ!" , "ПОШЛИ ПОМОЖЕШЬ!"};
+    private int counter = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showText(mainView);
 
-
-
+        text = (TextView) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+        text.setVisibility(View.VISIBLE);
+        click();
 
     }
 
 
-    public void showText(View view) {
-        Toast.makeText(this, "О! ЗДАРОВА ЧЕЛИК! ТУТ КОРОЧЕ НАДО ОДНОГО ЧУВАКА СОБРАТЬ" +
-                "ПОШЛИ, ПОМОЖЕШЬ!", Toast.LENGTH_LONG).show();
+    void click(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                int index = counter % textArray.length;
+                text.setText(textArray[index]);
+                if (counter >= 4){
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+
+
 
 
 
