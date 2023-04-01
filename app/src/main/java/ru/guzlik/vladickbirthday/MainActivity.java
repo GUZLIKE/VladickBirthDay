ccb6e3f57e1d2363362f868e3765acdd2cad1aa9
GUZLIKE <vinnickow.e@yandex.ru>
2023-04-01 14:27:39 +0300

    REMOVED BUTTON

--- app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
+++ app/src/main/java/ru/guzlik/vladickbirthday/MainActivity.java
@@ -7,18 +7,21 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
-    Button button;
+
+    ImageView creature;
 
     private String[] textArray = {"О! ЗДОРОВА! ТУТ ОДНОГО ЧЕЛА НАДО СОБРАТЬ!" , "ПОШЛИ ПОМОЖЕШЬ!"};
     private int counter = 1;
 
 
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +29,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         text = (TextView) findViewById(R.id.text);
-        button = (Button) findViewById(R.id.button);
+        creature = (ImageView) findViewById(R.id.debil);
         text.setVisibility(View.VISIBLE);
         click();
+        showToast();
 
     }
 
 
-    void click(){
-        button.setOnClickListener(new View.OnClickListener() {
+    void click() {
+        creature.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 counter++;
                 int index = counter % textArray.length;
                 text.setText(textArray[index]);
-                if (counter >= 4){
+                if (counter >= 4) {
                     Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                     startActivity(intent);
                 }
@@ -48,8 +52,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
-
+    public void showToast(){
+        Toast.makeText(this, "НАЖМИ НА ЧЕЛОВЕЧКА ЧТОБЫ НАЧАТЬ", Toast.LENGTH_LONG).show();
+    }
 
 
 
