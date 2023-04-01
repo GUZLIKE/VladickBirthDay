70ae844e305c3a349486d44e2dfd2ef09cab819d
Lekner89 <you@example.com>
2023-04-01 21:33:51 +0400

    ADDED ANOTHER "DEBIL2"
    ADDED ANOTHER ARRAY OF HIS DIALOGUE
    
    ADDED LOGIC APPEARANCE AND REPLACEMENT OF OUR "DEBIL2"
    
    ADDED ANOTHER FIELD "TASK" WHERE IT IS NECESSARY TO SOLVE THE TASK LOGIC IS NOT ADDED YET

--- app/src/main/java/ru/guzlik/vladickbirthday/SecondActivity.java
+++ app/src/main/java/ru/guzlik/vladickbirthday/SecondActivity.java
@@ -16,38 +16,49 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-    TextView text2;
+    TextView text2, text3;
 
-    ImageView creature, star;
+    ImageView creature,creatureSecond, star;
     EditText password;
 
+    EditText task;
 
-    private String[] textArray = {"Вот и ты на месте", "Перед тобой ты видишь 'Мысли вышего существа'","Нажми на них и взломай"};
+
+    private String[] textArray = {"Вот и ты на месте", "Перед тобой ты видишь 'Мысли вышего существа'", "Нажми на них и взломай"};
+    private String[] textArrayTask = {"Я думал ты знаешь пароль", "Нужен другой способ взломать", "Попробуй вот это "};
     private int counter = -1;
+    private int counterTask = -1;
 
 
 
-    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
 
         text2 = (TextView) findViewById(R.id.text2);
+        text3 = (TextView) findViewById(R.id.text3);
         creature = (ImageView) findViewById(R.id.debil2);
+        creatureSecond = (ImageView) findViewById(R.id.debil3);
         star = (ImageView) findViewById(R.id.star);
         password = (EditText) findViewById(R.id.password);
+        creatureSecond.setVisibility(View.GONE);
+        task = (EditText) findViewById(R.id.task);
 
+        task.setVisibility(View.GONE);
 
         password.setVisibility(View.GONE);
         star.setEnabled(false);
         text2.setVisibility(View.VISIBLE);
+        text3.setVisibility(View.VISIBLE);
         click();
         showToast();
         clickStar();
         passwordInputListener();
+        clickTask();
 
     }
+
     void click() {
         creature.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,7 +66,7 @@ public class SecondActivity extends AppCompatActivity {
                 counter++;
                 int index = counter % textArray.length;
                 text2.setText(textArray[index]);
-                if (counter >= 3){
+                if (counter >= 3) {
                     text2.setText("");
                     star.setEnabled(true);
                 }
@@ -64,24 +75,29 @@ public class SecondActivity extends AppCompatActivity {
         });
     }
 
-    public void showToast(){
+    public void showToast() {
         Toast.makeText(this, "НАЖМИ НА ЧЕЛОВЕЧКА ЧТОБЫ НАЧАТЬ", Toast.LENGTH_LONG).show();
     }
 
 
-
-    void passwordInputListener(){
+    void passwordInputListener() {
         password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE){
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
                     String enteredPassword = password.getText().toString();
-                    if (!TextUtils.isEmpty(enteredPassword)){
+                    if (!TextUtils.isEmpty(enteredPassword)) {
                         text2.setText("ТЫ ЧЁ ЕБЛАН? ТЫ РЕАЛЬНО ДУМАЛ ЧТО У НЕГО ТАКОЙ ПАРОЛЬ?");
                         password.setVisibility(View.GONE);
+                        creature.setVisibility(View.GONE);
+                        creatureSecond.setVisibility(View.VISIBLE);
+
+
                     } else {
                         text2.setText("ПОСОСИ");
-                        password.setVisibility(View.GONE);
+                        creature.setVisibility(View.GONE);
+                        creatureSecond.setVisibility(View.VISIBLE);
+
                     }
                     return true;
                 }
@@ -91,21 +107,39 @@ public class SecondActivity extends AppCompatActivity {
     }
 
 
-    void clickStar(){
+    void clickStar() {
+
         star.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    password.setVisibility(View.VISIBLE);
-                    password.requestFocus();
-                    InputMethodManager imm = (InputMethodManager)
-                            getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.showSoftInput(password,InputMethodManager.SHOW_IMPLICIT);
+                password.setVisibility(View.VISIBLE);
+                password.requestFocus();
+                InputMethodManager imm = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(password, InputMethodManager.SHOW_IMPLICIT);
             }
         });
     }
 
+    void clickTask() {
+        creatureSecond.setOnClickListener(new View.OnClickListener() {
+           @Override
+        public void onClick(View v) {
+        text2.setText("");
+        counterTask++;
+        int index = counterTask % textArrayTask.length;
+        text3.setText(textArrayTask[index]);
+        if (counterTask >= 3) {
+        text3.setText("");
+                }
 
+               }
+            }
 
+        );
+
+    }
 }
+
 
 
