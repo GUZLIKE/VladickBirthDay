00f69bdf6f0fe1c64c04319619508137868aafed
GUZLIKE <vinnickow.e@yandex.ru>
2023-04-02 17:30:18 +0300

    ADDED THIRD ACTIVITY

---
+++ app/src/main/java/ru/guzlik/vladickbirthday/ThirdActivity.java
@@ -0,0 +1,14 @@
+package ru.guzlik.vladickbirthday;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+    }
+}
